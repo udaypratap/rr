ca0383934e5af55a77d486cc5080543914dcbeb2
root <uday@infozion.in>
2021-06-21 17:18:43 +0530

    update email tempate html

--- resources/views/emails/xkcdmail.blade.php
+++ resources/views/emails/xkcdmail.blade.php
@@ -9,11 +9,7 @@
             <td data-color="Text 01" data-size="Text 01" data-min="5"
                 data-max="50" class="td-pad10-wide" align="center"
                 style="padding:0px 20px 0px 20px; font-weight:400; font-size:13px; letter-spacing:0.025em; line-height:26px; color:#51636f; font-family:'Poppins', sans-serif; mso-line-height-rule: exactly;">
-                @php
-                    echo '<pre>';
-                        print_r($maildata['message']);
-                    echo '</pre>';
-                @endphp
+                    {!! $maildata['message'] !!}
                     <br><br>
                     <img src="{{ $maildata['imageurl'] }}" />
             </td>
